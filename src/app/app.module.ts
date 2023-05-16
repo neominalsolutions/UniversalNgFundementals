da657be72dc96557349d372a217b88a48be32129
alptekin-mert <mrtlptkn45@gmail.com>
2023-05-16 11:08:40 +0300

    Module geliştirildi.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,8 @@ import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { BlockButtonComponent } from './components/block-button/block-button.component';
 import { SelectorComponent } from './components/selector/selector.component';
-
+import { ButtonModule } from 'primeng/button';
+import { UniModule } from './components/uni.module';
 // @ile tanımlanmış yapılar bir sınıfın angular tarafında farklı şekilde hizmet vermesini sağlar decorator denir
 
 @NgModule({
@@ -24,12 +25,12 @@ import { SelectorComponent } from './components/selector/selector.component';
     HomePageComponent,
     AboutPageComponent,
     ContactPageComponent,
-    BlockButtonComponent,
-    SelectorComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule, // Bir module başka bir module altındaki bileşenleri referans aalbilir. Dışarıdan npm i ile yüklenen paketlere ait modulleri tanımlarız.
+    ButtonModule,
+    UniModule,
   ],
   providers: [], // uygulama içerisindeki hizmetlerimiz buraya yazıyoruz
   bootstrap: [AppComponent], // uygulama ilk bu component çalıştır
